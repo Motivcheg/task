b6078560b2dac8a8b3f8f7a3fcfee64c4d5c8ae6
Max <lomanuy@gmail.com>
2022-09-01 18:51:56 +0300

    ready

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,13 +1,36 @@
 ﻿Console.WriteLine("Введите число: ");
-string num = Convert.ToString(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
 
-Console.WriteLine(num[2]);
 
-int index = num.Length;
-int find = index;
-Console.WriteLine(index);
 
-if (find < 3)
+if (num < 100)
 {
-    Console.WriteLine("Нет третьей цифры");
+    Console.WriteLine("Нет третьего числа");
 }
+
+if (num < 1000 && num >= 100)
+{
+    Console.WriteLine(num % 100/10);
+}
+
+if (num < 10000 && num >= 1000)
+{
+    Console.WriteLine(num % 1000 % 100/10);
+}
+
+if (num < 100000 && num >= 10000)
+{
+    Console.WriteLine(num % 10000 % 1000/100);
+}
+
+if (num < 1000000 && num >= 100000)
+{
+    Console.WriteLine(num % 100000 % 10000/1000);
+}
+
+if (num < 10000000 && num >= 1000000)
+{
+    Console.WriteLine(num % 1000000 % 100000/10000);
+}
+
+// И так далее.. если есть другой способ, подскажите, ломал голову долго, очень хочу знать, наверняка существует способ короче, о котором я не знаю, спасибо!
